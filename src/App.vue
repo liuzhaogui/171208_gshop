20af738742e61c343fd9d8ef2c0f42bc35ea8cdb
liujiu1 <2648263739@qq.com>
2020-07-02 09:54:36 +0800

    七月二

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,28 @@
 <template>
   <div id="app">
-	<router-view/>
-	<footer-guide v-show="$route.meta.showFooter"/>
+    <router-view />
+    <footer-guide v-show="$route.meta.showFooter" />
   </div>
 </template>
 
 <script>
-	import FooterGuide from './components/FooterGuide/FooterGuide.vue'
-	export default{
-		components:{
-			FooterGuide
-		}
-	}
+	import FooterGuide from "./components/FooterGuide/FooterGuide.vue";
+	// 先在App.vue中引入封装的接口函数
+	import { reqFoodCategorys } from "./api";
+	// 然后再调用接口，测试打印数据
+	export default {
+ 	  mounted() {
+		this.$store.dispatch('getAddress')
+  	},
+  	components: {
+    	FooterGuide
+ 	 }
+	};
 </script>
 <style lang="stylus" rel="stylesheet/stylus">
-	.app
-		width 100%
-		height 100%
-		background #f5f5f5
+.app {
+  width: 100%;
+  height: 100%;
+  background: #f5f5f5;
+}
 </style>
---
+++ src/store/state.js
@@ -0,0 +1,7 @@
+export default {
+    latitude: 40.100388, //纬度
+    longitude: 116.36867, //经度
+    address: {}, //地址相关信息对象
+    categorys: [], //食品分类数组
+    shops: []   //商家数组
+}
